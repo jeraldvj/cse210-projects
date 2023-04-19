f9f643040af89928c3a179ac15edc0fa51b1532e
jeraldvj <jeraldvj@byui.edu>
2023-04-19 18:34:45 -0400

    Learning Activity Prep 2 + Str.Challenge completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade in percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter;
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80 )
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70 )
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60 )
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // This part of the code assigns a sign after the degree letter, as long as it is not F or a + is not assigned if it is A.
+        int mod = percentage % 10;
+        string sign = "";
+        if (mod >= 7 && letter != "A")
+        {
+            if (letter != "F")
+            {
+                sign = "+";
+            }
+        }
+        else if (mod < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+
+        Console.Write($"\nYour grade letter is {letter}{sign}");
+
+        // This condition allows displaying the message according to its grade percentage.
+        if (percentage >= 70)
+        {
+            Console.WriteLine($"\nCongratulation for your grade.");
+        }
+        else
+        {
+            Console.WriteLine($"\nYou can do better next semester, don't be discouraged.");
+        }
     }
 }
