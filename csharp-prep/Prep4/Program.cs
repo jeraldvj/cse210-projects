0394541d83ce36c3a048ea00b5cb0d5afdd0fa67
jeraldvj <jeraldvj@byui.edu>
2023-04-27 09:43:02 -0400

    Learning Activity - Prep 4 + stre.chall. completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int d_number = 1;
+        float sum = 0;
+        int max = 0;
+        int small = 1000;
+        int counter = 0;
+        List<int> numbers = new List<int>();
+        while (d_number !=0){
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            d_number = int.Parse(userInput);
+            
+            sum += d_number;
+            if (max < d_number){
+                max = d_number;
+            }
+            if(d_number > 0 && d_number < small){
+                small = d_number;
+            }
+
+            if (d_number != 0){
+                ++counter;
+                numbers.Add(d_number);
+            }
+        }
+        float aver = sum/counter;
+        numbers.Sort();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {aver}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest positive number is: {small}");
+        Console.WriteLine($"The sorted list is: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
