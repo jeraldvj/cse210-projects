b7e6bd3ffbb5493470eb4eb23906b30a3e1a84a9
jeraldvj <jeraldvj@byui.edu>
2023-05-31 17:02:08 -0400

    Inheritance Learning Activity Completed

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textbook, string problems) : base (name, topic)
+    {
+        _textbookSection = textbook;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList(){
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment mathAssign1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssign1.GetSummary());
+        Console.WriteLine(mathAssign1.GetHomeworkList());
+
+        WritingAssignment writingAssign1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssign1.GetSummary());
+        Console.WriteLine(writingAssign1.GetWritingInformation());
+
+        
+
     }
 }
