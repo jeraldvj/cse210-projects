2eb4999ba05733d1b810554324009da1cab2c07b
jeraldvj <jeraldvj@byui.edu>
2023-06-14 22:12:24 -0400

    Polymorphism Learning Activity Completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("Green",4);
+
+        Rectangle r1 = new Rectangle("Red", 4, 8);
+
+        Circle c1 = new Circle("Yellow",4);
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(s1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        foreach (Shape sh in shapes)
+        {
+            Console.WriteLine($"The {sh.GetColor()} shape has an area of {sh.GetArea()}.");
+        }
+
     }
 }
